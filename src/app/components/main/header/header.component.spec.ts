72e03fa002de7321735097e30d2739d7dea9ed55
Oscar Wellner <oscar.wellner@gmail.com>
2021-06-21 23:27:09 +0200

    Updated tests for reformatting app components

--- src/app/components/main/header/header.component.spec.ts
+++ src/app/components/main/header/header.component.spec.ts
@@ -3,23 +3,25 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
+
   let fixture: ComponentFixture<HeaderComponent>;
+
+  let component: HeaderComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
+
+    component = fixture.debugElement.componentInstance;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should create', () => expect(component).not.toBeNull());
 });
