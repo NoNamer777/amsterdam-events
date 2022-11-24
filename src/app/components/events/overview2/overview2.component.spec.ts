82d2e2cefb657bf0a95e50e5301f6dd075e4bf1a
Oscar Wellner <oscar.wellner@gmail.com>
2022-11-24 12:44:31 +0100

    Add test for events-overview2 component

--- src/app/components/events/overview2/overview2.component.spec.ts
+++ src/app/components/events/overview2/overview2.component.spec.ts
@@ -1,25 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { EventsOverview2ComponentHarness } from '../../../../testing';
 import { Overview2Component } from './overview2.component';
 
-describe('Overview2Component', () => {
-  let component: Overview2Component;
-  let fixture: ComponentFixture<Overview2Component>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ Overview2Component ]
+fdescribe('Overview2Component', () => {
+    @Component({
+        template: `<app-overview2></app-overview2>`,
     })
-    .compileComponents();
-  });
+    class TestComponent {}
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(Overview2Component);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    async function setupTestEnvironment() {
+        TestBed.configureTestingModule({
+            imports: [CommonModule],
+            declarations: [Overview2Component, TestComponent],
+        });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        const harnessLoader = TestbedHarnessEnvironment.loader(TestBed.createComponent(TestComponent));
+
+        return {
+            harness: await harnessLoader.getHarness(EventsOverview2ComponentHarness),
+        };
+    }
+
+    it('should show the events', async () => {
+        const { harness } = await setupTestEnvironment();
+
+        expect((await harness.getEventElements()).length).toEqual(10);
+        expect(await harness.isEventDetailsPlaceholderVisible()).toBeTrue();
+        expect(await harness.isEventDetailsVisible()).toBeFalse();
+        expect(await harness.hasEventSelected()).toBeFalse();
+    });
 });
