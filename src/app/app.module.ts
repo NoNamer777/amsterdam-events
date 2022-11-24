2b74f198b4387d04f72a9e18413eb2535af0780a
Menno Wellner <menno.wellner@hotmail.com>
2022-11-24 14:16:33 +0100

    AE-7: Adds basic table for event details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { Details2Component } from './components/events/details2/details2.component';
 import { Overview1Component } from './components/events/overview1/overview1.component';
 import { Overview2Component } from './components/events/overview2/overview2.component';
 import { HeaderComponent } from './components/main/header/header.component';
 import { HomeComponent } from './components/main/home/home.component';
 
 @NgModule({
-    declarations: [AppComponent, HomeComponent, HeaderComponent, Overview1Component, Overview2Component],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        HeaderComponent,
+        Overview1Component,
+        Overview2Component,
+        Details2Component,
+        Details2Component,
+    ],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent],
