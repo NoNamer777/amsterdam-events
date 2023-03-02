e553ed63a4e3e2a3da4dec3e6b3de09dcd11fdce
Menno Wellner <menno.wellner@hotmail.com>
2023-03-02 14:27:21 +0100

    AE-9: Adds tests for details2Component

--- src/testing/harnesses/index.ts
+++ src/testing/harnesses/index.ts
@@ -1,2 +1,3 @@
+export * from './components/events-details2-component.harness';
 export * from './components/events-overview1-component.harness';
 export * from './components/events-overview2-component.harness';
