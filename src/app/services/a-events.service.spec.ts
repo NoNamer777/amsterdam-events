79483b612279317518a7d3c2d2e7c250ac0e3ba8
Menno Wellner <menno.wellner@hotmail.com>
2023-06-08 14:52:59 +0200

    AE-41: Creats AEventsService with CRUD operations

--- src/app/services/a-events.service.spec.ts
+++ src/app/services/a-events.service.spec.ts
@@ -1,16 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AEvent, AEventStatus } from '../models/a-event.model';
 import { AEventsService } from './a-events.service';
 
 fdescribe('AEventsService', () => {
     let service: AEventsService;
-
-    const MAX_EVENT_PARTICIPANTS = 8000;
-    const MAX_EVENT_PARTICIPATION_FEE = 50;
-    const RANDOM_GENERATED_EVENTS = 10;
-
-    randomEvents();
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
@@ -21,7 +14,9 @@ fdescribe('AEventsService', () => {
         expect(service.getAll()).toHaveSize(10);
     });
 
-    it('should return an Aevent by ID', () => {});
+    it('should return an Aevent by ID', () => {
+        expect(service.)
+    });
 
     it('should throw an Error when AEvent is not found by ID', () => {});
 
@@ -36,47 +31,4 @@ fdescribe('AEventsService', () => {
     it('should remove an AEvent', () => {});
 
     it('should throw an error when removing an AEvent that does not exist.', () => {});
-
-    function randomEvents(): void {
-        for (let i = 0; i < RANDOM_GENERATED_EVENTS; i++) {
-            const event = generateEvent();
-            console.log(event);
-            service.add(event);
-        }
-    }
-
-    function generateEvent(): AEvent {
-        const newEvent = new AEvent();
-        newEvent.title = `The Fantastic event-${newEvent.id}`;
-        newEvent.status = randomEventStatus();
-        newEvent.start = randomDate();
-        newEvent.end = randomDate(newEvent.start);
-        newEvent.hasTickets = Math.random() >= 0.5;
-        newEvent.participationFee = newEvent.hasTickets ? Math.floor(Math.random() * MAX_EVENT_PARTICIPATION_FEE) : 0;
-        newEvent.maxParticipants = newEvent.hasTickets ? Math.floor(Math.random() * MAX_EVENT_PARTICIPANTS) : 0;
-
-        return newEvent;
-    }
-
-    function randomEventStatus(): AEventStatus {
-        const rndNum: number = Math.floor(Math.random() * 3);
-
-        const statuses = Object.keys(AEventStatus) as AEventStatus[];
-
-        return AEventStatus[statuses[rndNum]];
-    }
-
-    function randomDate(before?: Date): Date {
-        const rndDay: number = Math.floor(Math.random() * 31);
-        const rndMonth: number = Math.floor(Math.random() * 13);
-        const rndYear: number = Math.floor(Math.max(2020, Math.floor(Math.random() * 20) + 2020));
-        const rndHour: number = Math.floor(Math.random() * 24);
-
-        const generatedDate = new Date(rndYear, rndMonth, rndDay, rndHour);
-
-        if (before && generatedDate < before) {
-            return randomDate(before);
-        }
-        return generatedDate;
-    }
 });
--- src/app/services/a-events.service.ts
+++ src/app/services/a-events.service.ts
@@ -1,49 +1,107 @@
 import { Injectable } from '@angular/core';
-import { AEvent } from '../models/a-event.model';
+import { AEvent, AEventStatus } from '../models/a-event.model';
 
-interface AEventsDB {
-    [eventId: number]: AEvent;
-}
+const MAX_EVENT_PARTICIPANTS = 8000;
+const MAX_EVENT_PARTICIPATION_FEE = 50;
+const RANDOM_GENERATED_EVENTS = 10;
 
 @Injectable({
     providedIn: 'root',
 })
 export class AEventsService {
-    public aEvents: AEventsDB;
+    public events: AEvent[] = [];
 
-    public getAll(): AEvent[] {
-        return Object.values(this.aEvents);
+    constructor() {
+        this.addRandomAEvent();
     }
 
-    public getById(eventId: number): AEvent {
-        if (!this.aEvents[eventId]) throw new Error(`Event with ID: '${eventId}' is not found.`);
-        return this.aEvents[eventId];
+    /**
+     * Returns all events in events
+     */
+    getAll(): AEvent[] {
+        return this.events;
     }
 
-    public add(aEvent: AEvent): number {
-        if (this.aEvents[aEvent.id]) throw new Error(`Event with ID: '${aEvent.id}' already exists.`);
-
-        this.aEvents[aEvent.id] = aEvent;
-        return aEvent.id;
+    /**
+     * Add a new event.
+     * @param newEvent {AEvent}
+     * @return {index} The index of the event in the list of events.
+     */
+    add(newEvent: AEvent): number {
+        this.events = [...this.events, newEvent];
+        return this.events.indexOf(newEvent);
     }
 
-    public update(eventId: number, aEvent: AEvent): void {
-        if (!this.aEvents[eventId]) throw new Error(`Event with ID: '${eventId}' is not found.`);
-        if (this.aEvents[eventId].id !== aEvent.id) {
-            throw new Error(
-                `Could not update Event with ID: '${eventId}' with data from Event with ID: '${aEvent.id}'.`,
-            );
-        }
-        this.aEvents[eventId] = aEvent;
+    /**
+     * replace the identified event with the provided
+     * @param index of the event which needs to be updated
+     * @param aEvent data to update the existing event with
+     */
+    update(index: number, aEvent: AEvent): void {
+        // TODO
+        this.events[index] = aEvent;
     }
 
-    public remove(eventId: number): AEvent {
-        if (!this.aEvents[eventId]) {
-            throw new Error(`Could not remove event with ID: '${eventId}' because it does not exist.`);
-        }
-        const removedEvent = this.aEvents[eventId];
-
-        delete this.aEvents[eventId];
+    /**
+     * remove the identified event from the collection
+     * and return the removed instance
+     * @param index of the event to remove
+     */
+    remove(index: number): AEvent {
+        const removedEvent = this.events.at(index);
+        this.events = this.events.splice(index, 1);
         return removedEvent;
+    }
+
+    /** Generate {@link RANDOM_GENERATED_EVENTS} number of "random" events */
+    private addRandomAEvent(): void {
+        for (let i = 0; i < RANDOM_GENERATED_EVENTS; i++) {
+            this.events.push(this.generateEvent());
+        }
+    }
+
+    /**
+     * Generates a single event with data.
+     * @return AEvent which is generated.
+     */
+    private generateEvent(): AEvent {
+        const newEvent = new AEvent();
+
+        newEvent.title = `The Fantastic event-${newEvent.id}`;
+        newEvent.status = this.randomEventStatus();
+        newEvent.start = this.randomDate();
+        newEvent.end = this.randomDate(newEvent.start);
+        newEvent.hasTickets = Math.random() >= 0.5;
+        newEvent.participationFee = newEvent.hasTickets ? Math.floor(Math.random() * MAX_EVENT_PARTICIPATION_FEE) : 0;
+        newEvent.maxParticipants = newEvent.hasTickets ? Math.floor(Math.random() * MAX_EVENT_PARTICIPANTS) : 0;
+
+        return newEvent;
+    }
+
+    /** Generates a 'random' event status which can be any value of {@link AEventStatus} */
+    private randomEventStatus(): AEventStatus {
+        const rndNum: number = Math.floor(Math.random() * 3);
+
+        const statuses = Object.keys(AEventStatus) as AEventStatus[];
+
+        return AEventStatus[statuses[rndNum]];
+    }
+
+    /**
+     * Generates a 'random' date which is after the before parameter
+     * @param before date to generate before.
+     */
+    private randomDate(before?: Date): Date {
+        const rndDay: number = Math.floor(Math.random() * 31);
+        const rndMonth: number = Math.floor(Math.random() * 13);
+        const rndYear: number = Math.floor(Math.max(2020, Math.floor(Math.random() * 20) + 2020));
+        const rndHour: number = Math.floor(Math.random() * 24);
+
+        const generatedDate = new Date(rndYear, rndMonth, rndDay, rndHour);
+
+        if (before && generatedDate < before) {
+            return this.randomDate(before);
+        }
+        return generatedDate;
     }
 }
